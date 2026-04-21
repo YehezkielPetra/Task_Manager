7015e9c8cf4ce1b46ef1b0a97d897b8fca5757fd
Yehezkiel Petra <Yehezkielpetra.ypk@gmail.com>
2026-04-21 13:35:09 +0700

    fix port for deployment

--- server/server.js
+++ server/server.js
@@ -117,4 +117,5 @@ app.delete('/api/tasks/:id', authenticateToken, async (req, res) => {
     } catch (error) { res.status(500).json(error); }
 });
 
-app.listen(5000, () => console.log(`🚀 Server running on port 5000`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
